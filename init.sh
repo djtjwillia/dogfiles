e2a7ec61518c863d62b35154c2c7b655aeac617d
Taylor Williams <williams.tjoseph@gmail.com>
2022-11-04 13:54:39 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 brew bundle
-chmod +x ./scripts/init-dot-files.sh ./scripts/init-dot-dirs.sh
-./init-dot-files.sh -p; ./init-dot-dirs.sh -p
+chmod +x ./scripts/init-dot-files.sh ./scripts/init-vscode.sh
+./init-dot-files.sh -t; ./scripts/init-vscode.sh -t
