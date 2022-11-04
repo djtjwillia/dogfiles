48937c51c1db9430a9a7291004b292a2e69984ca
Taylor J Williams <23586790+djtjwillia@users.noreply.github.com>
2022-11-04 15:06:03 -0600

    feat: fixing up the zshrc and init scripts

--- scripts/init-vscode.sh
+++ scripts/init-vscode.sh
@@ -3,14 +3,14 @@
 if [ $1 == "-s" ]
 then 
     echo "\033[1;32mSetting up vscode resources\033[0m"
-    cp ../vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-    cat extensions.list | xargs -L1 code --install-extension
+    cp ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+    cat ./vscode/extensions.list | xargs -L1 code --install-extension
 elif [ $1 == "-t" ]
 then
     echo " \033[1;32mTest vscode setup.\n"\
-         "\033[0;32mTest settings.json will be here:\033[3;93m  $HOME/Library/Application\ Support/Code/User/settings.json\033[0m"
+         "\033[0;32mTest settings.json will be here:\033[3;93m  $HOME/Library/Application\ Support/Code/User/settings.json\033[0m"\
          "\033[0;32mReview extensions:\033[0m"
-         cat extensions.list
+         cat ./vscode/extensions.list
 else
     echo " \033[1;31mindex-dot-files usage:\n"\
          "\033[0;31mOne of the following is required as an aguement:\n"\
