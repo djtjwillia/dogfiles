bb2c6170f4f86626d7dc3691b56da1b1e160c5b8
DJTJWillia <williams.tjoseph@gmail.com>
2022-11-04 15:06:03 -0600

    feat: fixing up the zshrc and init scripts

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-brew bundle
-chmod +x ./scripts/init-dot-files.sh ./scripts/init-vscode.sh
-./init-dot-files.sh -t; ./scripts/init-vscode.sh -t
+# brew bundle
+chmod +x  ./scripts/init-dot-files.sh ./scripts/init-vscode.sh
+./scripts/init-dot-files.sh -t; ./scripts/init-vscode.sh -t
