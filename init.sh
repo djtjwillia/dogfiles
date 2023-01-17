ad835dca23dacaa613bc915523d9ccce12ce67ac
Taylor Williams <williams.tjoseph@gmail.com>
2023-01-16 21:56:31 -0700

    fix: moving in the install of oh-my-zsh and brew

--- init.sh
+++ init.sh
@@ -1,3 +1,17 @@
+if [ -d ~/.oh-my-zsh ]; then
+    echo "oh-my-zsh is installed"
+else
+    echo "oh-my-zsh is not installed"
+fi
+
+if ! command -v brew &> /dev/null
+then
+    echo "brew could not be found, installing"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "brew is installed"
+fi
+
 # brew bundle
 chmod +x  ./scripts/init-dot-files.sh ./scripts/init-vscode.sh
-./scripts/init-dot-files.sh -t; ./scripts/init-vscode.sh -t
+./scripts/init-dot-files.sh $1; ./scripts/init-vscode.sh $2
