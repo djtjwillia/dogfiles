a966f87852930b9a1b52df0dab9502f612f98763
Taylor J Williams <23586790+djtjwillia@users.noreply.github.com>
2023-11-28 08:48:02 -0700

    fix: fixing zsh and making git ssh config optional

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 if [ -d ~/.oh-my-zsh ]; then
     echo "oh-my-zsh is installed"
 else
     echo "oh-my-zsh is not installed"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 if ! command -v brew &> /dev/null
